fed5fba140acd20b9d831423df6776868ea81b59
Eli Lemons <me@elilemons.com>
2024-06-27 16:04:23 -0400

    The Stepper solution

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,5 +1,6 @@
-import { Stepper, Step, StepIndicator, StepStatus, StepIcon, StepNumber, StepTitle, StepDescription, StepSeparator, Box, useSteps } from '@chakra-ui/react';
+import { Stepper, Step, StepIndicator, StepStatus, StepIcon, StepNumber, StepTitle, StepDescription, StepSeparator, Box, useSteps, Progress, NumberInput, NumberInputField, NumberDecrementStepper, NumberIncrementStepper, NumberInputStepper } from '@chakra-ui/react';
 import * as React from "react"
+import { TbSailboat2 } from "react-icons/tb";
 
 // TODO Remove test code, use actual type
 type Status = {
@@ -16,7 +17,7 @@ const steps: Status[] = [
   },
   {
     label: "Rising",
-    min: 10,
+    min: 11,
     max: 90
   },
   {
@@ -32,23 +33,48 @@ const steps: Status[] = [
 ]
 
 const ProgressBar: React.FC = () => {
+  const findActiveStep = (value: number) => {
+    return steps.reduce((activeStep, currentStep, index) => {
+      if (value >= currentStep.min && value <= currentStep.max) {
+        return index;
+      }
+      return activeStep;
+    }, 0);
+  }
+
   const { activeStep, setActiveStep } = useSteps({
     index: 0,
     count: steps.length
   })
 
+  const [ currentValue, setCurrentValue ] = React.useState(0)
 
-  return (
+  React.useEffect(() => {
+    if (currentValue <= steps[activeStep].max || currentValue >= steps[activeStep].min) {
+      setActiveStep(findActiveStep(currentValue))
+    }
+
+  }, [activeStep, currentValue, setActiveStep])
+
+  return (<>
+    <NumberInput defaultValue={currentValue} min={0} onChange={(value) => setCurrentValue(Number(value))} step={10}>
+      <NumberInputField />
+      <NumberInputStepper>
+        <NumberIncrementStepper />
+        <NumberDecrementStepper />
+      </NumberInputStepper>
+    </NumberInput>
+
     <Stepper size='lg' index={activeStep} orientation='vertical' height='400px'>
     {steps.map((step, index) => (
-      <Step key={index} onClick={() => setActiveStep(index)} >
+      <Step key={index} >
         <StepIndicator>
           <StepStatus
-            complete={<StepIcon />}
-            incomplete={<StepNumber />}
-            active={<StepNumber />}
+            active={<TbSailboat2 />}
           />
         </StepIndicator>
+
+
 
         <Box flexShrink='0'>
           <StepTitle>{step.label}</StepTitle>
@@ -58,7 +84,8 @@ const ProgressBar: React.FC = () => {
         <StepSeparator />
       </Step>
     ))}
-  </Stepper>
+    </Stepper>
+    </>
   )
 }
 
